42edbe20f3aee7fde9c3e594a3d5c93c83a3372e
alex5523 <52035544+alex5523@users.noreply.github.com>
2023-12-17 17:54:18 +0100

    while loop will be rebuilt

--- Abgabe/newton_examples.py
+++ Abgabe/newton_examples.py
@@ -16,120 +16,183 @@ from newton_plots import plot_intersection
 # Example functions
 #############################
 
-# Example 1: Hyperbola and circle
-# Function
-def f1(x):
-    m1 = [0.0,0.0]
-    m2 = [0.0,0.0]
-    r1 = np.sqrt(2)
-    r2 = np.sqrt(1)
-    return np.array([(x[0] - m1[0])**2 + (x[1] - m1[1])**2 - r1**2,
-                     (x[0] - m2[0])**2 - (x[1] - m2[1])**2 - r2**2])
+# Example 1: Intersection of a circle and a hyperbola
+def ex1(x_init):
+    
+    # Function
+    def f1(x):
+        m1 = [0.0,0.0]
+        m2 = [0.0,0.0]
+        r1 = np.sqrt(2)
+        r2 = np.sqrt(1)
+        return np.array([(x[0] - m1[0])**2 + (x[1] - m1[1])**2 - r1**2,
+                         (x[0] - m2[0])**2 - (x[1] - m2[1])**2 - r2**2])
 
-# Jacobian of function
-def J1(x):
-    return np.array([[2*x[0], 2*x[1]],
-                     [2*x[0], -2*x[1]]])
+    # Jacobian of function
+    def J1(x):
+        return np.array([[2*x[0], 2*x[1]],
+                         [2*x[0], -2*x[1]]])
+    
+    # Initial guess
+    x_init = x_init
 
-# Initial guess
-x_init = [1.0, 1.0]
+    # MYSOLVER
+    x, k_end = newton_method1(f1, J1, x_init)
+    
+    # FSOLVER
+    sol = fsolve(f1, x_init)  # Löse nichtlineares Gleichungssystem
+    
+    print("Example 1: Intersection of a circle and a hyperbola", x)
+    print("x_init: ", x_init)
+    print("My Newton Method Solution:\n", x)
+    print("Number of Iterations:", k_end)
+    print("fsolve:\n", sol)
+    print("Mysolution = fsolve ? ", np.allclose(sol, x))
+    
+    # Visualize the solution
+    x1_vals = np.linspace(-3, 3, 400)
+    x2_vals = np.linspace(-3, 3, 400)
+    #plot_intersection(f1, J1, x_init, 'Intersection of a Circle and a Hyperbola', x1_vals, x2_vals)
 
-# MYSOLVER
-x, k_end = newton_method1(f1, J1, x_init)
+    return
 
-#print("EX1: A circle and a hyperbola", x)
-#print("Newton Method Solution:", x)
-#print("Number of Iterations:", k_end)
-
-# FSOLVER
-sol = fsolve(f1, x_init)  # Löse nichtlineares Gleichungssystem
-#print("fsolve =", sol)
-
-
-
-# Example 2: 2 circles
-# Function
-def f2(x):
-    m1 = [0.0,0.0]
-    m2 = [0.0,5.0]
-    r1 = 3
-    r2 = 3
-    return np.array([(x[0] - m1[0])**2 + (x[1] - m1[1])**2 - r1**2,
-                     (x[0] - m2[0])**2 + (x[1] - m2[1])**2 - r2**2])
-
-# Jacobian of function
-def J2(x):
-    return np.array([[2*x[0], 2*x[1]],
-                     [2*x[0], 2*x[1] - 10]])
-
-# Initial guess
-x_init = [1.0, 4.0]
-
-# MYSOLVER
-x, k_end = newton_method1(f2, J2, x_init)
-
-#print("\nEx2: Two circles", x)
-#print("Newton Method Solution:", x)
-#print("Number of Iterations:", k_end)
-
-# FSOLVER
-sol = fsolve(f2, x_init)  # Löse nichtlineares Gleichungssystem
-#print("fsolve=", sol)
+# Debugging test example
+#x_init = [1.0, 1.0] # converges
+#x_init = [1.0, 1.0, 1.0] # wont cut dimension checks
+#x_init = np.array([[1.0, 1.0], [1.0, 1.0]]) # wont cut dimension checks
+#x_init = [1.0, 0.0] # Jacobian singular
+#ex1(x_init)
 
 
-def ex2():
+
+# Example 2: Two intersecting circles
+def ex2(x_init):
+    
+    # Function
+    def f2(x):
+        m1 = [0.0, 0.0]
+        m2 = [0.0, 5.0]
+        r1 = 3
+        r2 = 3
+        return np.array([(x[0] - m1[0])**2 + (x[1] - m1[1])**2 - r1**2,
+                         (x[0] - m2[0])**2 + (x[1] - m2[1])**2 - r2**2])
+
+    # Jacobian of function
+    def J2(x):
+        return np.array([[2*x[0], 2*x[1]],
+                         [2*x[0], 2*x[1] - 10]])
+
+    # Initial guess
+    #x_init = [1.0, 4.0] # converges
+
+    # MYSOLVER
+    x, k_end = newton_method1(f2, J2, x_init)
+
+    # FSOLVER
+    sol = fsolve(f2, x_init)  # Löse nichtlineares Gleichungssystem    
+    
+    # Print statements
+    print("Example 2: Two intersecting circles")
+    print("My Newton Method Solution:\n", x)
+    print("fsolve:\n", sol)
+    print("Mysolution = fsolve ? ", np.allclose(sol, x))
+    print("Number of Iterations:", k_end)
+
+    # Visualise the solution
     x1_vals = np.linspace(-6, 6, 400)
     x2_vals = np.linspace(-3, 8, 400)
-    plot_intersection(f2, J2, [1.0, 4.0], 'Intersection of Two Circles', x1_vals, x2_vals)
-    print("\nEx2: Two circles", x)
-    print("Newton Method Solution:", x)
+    #plot_intersection(f2, J2, x_init, 'Intersection of Two Circles', x1_vals, x2_vals)       
+    return
+
+x_init = [1.0, 1.0]
+ex2(x_init)
+
+# Example 3: Three intersecting spheres
+def ex3():
+    # Function
+    def f3(x):
+        m1 = [2.0, 0.0, -1.0]
+        m2 = [1.0, 2.0, 0.0]
+        m3 = [0.0, 0.0, -2.0]
+        r1 = 2.0
+        r2 = 2.0
+        r3 = 2.0
+        
+        return np.array([
+        (x[0] - m1[0])**2 + (x[1] - m1[1])**2 + (x[2] - m1[2])**2 - r1**2,
+        (x[0] - m2[0])**2 + (x[1] - m2[1])**2 + (x[2] - m2[2])**2 - r2**2,
+        (x[0] - m3[0])**2 + (x[1] - m3[1])**2 + (x[2] - m3[2])**2 - r3**2
+        ])
+    
+    # Jacobian of function
+    def J3(x):
+        
+        return np.array([
+        [2 * (x[0] - 2.0), 2 * (x[1] - 0.0), 2 * (x[2] - 1.0)],
+        [2 * (x[0] - 1.0), 2 * (x[1] - 2.0), 2 * (x[2] - 0.0)],
+        [2 * (x[0] - 0.0), 2 * (x[1] - 0.0), 2 * (x[2] - (-2.0))]
+        ])
+    
+    # Initial guess
+    #x_init = [1.0, 0.0, -1.0]
+    x_init = [-1.0, 0.0, 1.0] # am schnellsten!
+    #x_init = [-1.0, 0.0, -1.0]
+    
+    # MYSOLVER
+    x, k_end = newton_method1(f3, J3, x_init)
+    
+    # FSOLVER
+    sol = fsolve(f3, x_init)  # Löse nichtlineares Gleichungssystem
+    
+    # Print statements
+    print("Example 3: Three intersecting spheres")
+    print("My Newton Method Solution:\n", x)
     print("Number of Iterations:", k_end)
+    print("fsolve:\n", sol)
+    print("Mysolution = fsolve ? ", np.allclose(sol, x))
+    
     return
 
 
 
-
-
-
-# Example 3: Three spheres
-# Function
-def f3(x):
-    m1 = [2.0, 0.0, -1.0]
-    m2 = [1.0, 2.0, 0.0]
-    m3 = [0.0, 0.0, -2.0]
-    r1 = 2.0
-    r2 = 2.0
-    r3 = 2.0
+# Example 4: Two non intersecting circles
+def ex4(x_init):
     
-    return np.array([
-        (x[0] - m1[0])**2 + (x[1] - m1[1])**2 + (x[2] - m1[2])**2 - r1**2,
-        (x[0] - m2[0])**2 + (x[1] - m2[1])**2 + (x[2] - m2[2])**2 - r2**2,
-        (x[0] - m3[0])**2 + (x[1] - m3[1])**2 + (x[2] - m3[2])**2 - r3**2
-    ])
+    # Function
+    def f4(x):
+        m1 = [0.0, 0.0]
+        m2 = [0.0, 5.0]
+        r1 = 2
+        r2 = 2
+        return np.array([(x[0] - m1[0])**2 + (x[1] - m1[1])**2 - r1**2,
+                         (x[0] - m2[0])**2 + (x[1] - m2[1])**2 - r2**2])
 
-# Jacobian of function
-def J3(x):
-    return np.array([
-        [2 * (x[0] - 2.0), 2 * (x[1] - 0.0), 2 * (x[2] - 1.0)],
-        [2 * (x[0] - 1.0), 2 * (x[1] - 2.0), 2 * (x[2] - 0.0)],
-        [2 * (x[0] - 0.0), 2 * (x[1] - 0.0), 2 * (x[2] - (-2.0))]
-    ])
+    # Jacobian of function
+    def J4(x):
+        return np.array([[2*x[0], 2*x[1]],
+                         [2*x[0], 2*x[1] - 10]])
 
-# Initial guess
-#x_init = [1.0, 0.0, -1.0]
-x_init = [-1.0, 0.0, 1.0] # am schnellsten!
-#x_init = [-1.0, 0.0, -1.0]
+    # Initial guess
+    #x_init = [1.0, 4.0] # converges
 
+    # MYSOLVER
+    x, k_end = newton_method1(f4, J4, x_init)
 
-# MYSOLVER
-x, k_end = newton_method1(f3, J3, x_init)
+    # FSOLVER
+    sol = fsolve(f4, x_init)  # Löse nichtlineares Gleichungssystem    
+    
+    # Print statements
+    print("Example 4: Two non intersecting circles")
+    print("My Newton Method Solution:\n", x)
+    print("fsolve:\n", sol)
+    print("Mysolution = fsolve ? ", np.allclose(sol, x))
+    print("Number of Iterations:", k_end)
 
-#print("\nEX3: Three spheres", x)
-#print("Newton Method Solution:", x)
-#print("Number of Iterations:", k_end)
+    # Visualise the solution
+    x1_vals = np.linspace(-6, 6, 400)
+    x2_vals = np.linspace(-3, 8, 400)
+    plot_intersection(f4, J4, x_init, 'Intersection of Two Circles', x1_vals, x2_vals)       
+    return
 
-# FSOLVER
-sol = fsolve(f3, x_init)  # Löse nichtlineares Gleichungssystem
-#print("fsolve=", sol)
-
-
+x_init = [1.0, 0.0] 
+ex4(x_init)
--- Abgabe/newton_more_variables.py
+++ Abgabe/newton_more_variables.py
@@ -12,9 +12,22 @@ from scipy.linalg import qr
 from num1a_p1_main import back_sub
 
 # MYSOLVER
-def newton_method1(f, J, x0, TOL_res=1e-10, TOL_inc=1e-10, k_max=100):
-    x = np.array(x0, dtype=float)
+def newton_method1(f, J, x_init, tolerance = 1e-10, k_max=100):
+    
+    # Parameters/start initialisations
+    TOL_res = tolerance
+    TOL_inc = tolerance
+    x = np.array(x_init, dtype=float)
     k = 0   # count iterations
+    
+    # Dimension checks
+    if np.ndim(x_init) == 1:
+        n_unknowns = len(x_init)
+    else:
+        raise ValueError("Initial guess must be a 1D array (vector).")
+
+    if len(f(x)) != n_unknowns:
+        raise ValueError("The number of equations must be equal to the number of unknowns.")
 
     while k < k_max:
         res = f(x)  # Residual
@@ -30,7 +43,9 @@ def newton_method1(f, J, x0, TOL_res=1e-10, TOL_inc=1e-10, k_max=100):
         else:
             # Check for singular Jacobian matrix
             if np.linalg.det(jacobian) == 0:
+                print(k)
                 raise ValueError("Singular Jacobian matrix. Newton method may not converge.")
+                
             
             # Solve linear system using QR decomposition and backwards substitution
             Q, R = qr(jacobian)
@@ -42,7 +57,9 @@ def newton_method1(f, J, x0, TOL_res=1e-10, TOL_inc=1e-10, k_max=100):
         x += inc
         k += 1
     
-    raise ValueError("Newton method did not converge within the maximum number of iterations.")
+    else:
+        raise ValueError(f"Newton method did not converge for k_max = {k_max}.")
+
 
 
 
